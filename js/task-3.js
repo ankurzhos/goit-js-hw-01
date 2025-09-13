011818a3d28c3b2f262371fc487edb7f8e87f591
Anna K <kurzhos96@gmail.com>
2025-09-13 22:24:56 +0300

    add all hw tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,11 @@
-function getElementWidth (content, padding, border, Npx) {
+function getElementWidth (content, padding, border) {
+const contentValue = Number.parseFloat(content);
+const paddingValue = Number.parseFloat(padding);
+const borderValue = Number.parseFloat(border);
 
-}
+const totalWidth = contentValue + paddingValue *2 + borderValue *2;
+return totalWidth;
+};
 
 
 
